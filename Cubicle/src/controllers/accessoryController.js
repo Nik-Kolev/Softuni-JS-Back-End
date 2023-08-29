5e090e25435442a379d22b64accb7871fd77a870
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-08-29 21:26:05 +0300

    Added create Accessory functionality

--- Cubicle/src/controllers/accessoryController.js
+++ Cubicle/src/controllers/accessoryController.js
@@ -1,7 +1,15 @@
+const accessoryService = require('../services/accessoryService');
+
 const accessoryController = require('express').Router();
 
 accessoryController.get('/create', (req, res) => {
     res.render('accessory/create');
+});
+
+accessoryController.post('/create', async (req, res) => {
+    let data = req.body;
+    await accessoryService.createAccessory(data);
+    res.redirect('/');
 });
 
 module.exports = accessoryController;
---
+++ Cubicle/src/services/accessoryService.js
@@ -0,0 +1,3 @@
+const Accessory = require('../models/Accessory');
+
+exports.createAccessory = (data) => Accessory.create(data);

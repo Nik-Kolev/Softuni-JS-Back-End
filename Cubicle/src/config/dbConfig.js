8aa95659c6890ecc1136ca9d25148b0a588f5bdc
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-08-29 19:04:36 +0300

    Added mongoose DB

---
+++ Cubicle/src/config/dbConfig.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const uri = 'mongodb://127.0.0.1:27017/cubicle';
+
+async function dbConnect() {
+    await mongoose.connect(uri);
+}
+
+module.exports = dbConnect;
--- Cubicle/src/index.js
+++ Cubicle/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const dbConnect = require('./config/dbConfig');
 const expressConfig = require('./config/expressConfig');
 const hbsConfig = require('./config/hbsConfig');
 const router = require('./router');
@@ -8,6 +9,13 @@ const port = 3000;
 
 expressConfig(app);
 hbsConfig(app);
+
+dbConnect()
+    .then(() => console.log('DB Connected successfully!'))
+    .catch((err) => {
+        console.log('DB error', err);
+    });
+
 app.use(router);
 
 app.listen(port, () => console.log(`Server is working on ${port}`));

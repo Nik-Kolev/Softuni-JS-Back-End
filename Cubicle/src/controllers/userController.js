f6af865bb9e50786da9106b224be1d5dd27e6dc0
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-09-02 13:53:02 +0300

    validating and hashing the password

--- Cubicle/src/controllers/userController.js
+++ Cubicle/src/controllers/userController.js
@@ -1,7 +1,15 @@
+const userService = require('../services/userService');
+
 const userController = require('express').Router();
 
 userController.get('/register', async (req, res) => {
     res.render('user/register');
+});
+
+userController.post('/register', async (req, res) => {
+    const { username, password, repeatPassword } = req.body;
+    await userService.register({ username, password, repeatPassword });
+    res.redirect('/');
 });
 
 module.exports = userController;
---
+++ Cubicle/src/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: {
+        type: String,
+        // validate: {
+        //     validator: function (value) {
+        //         return this.repeatPassword === value;
+        //     },
+        //     message: `Password does not match!`,
+        // },
+    },
+});
+
+userSchema.virtual('repeatPassword').set(function (value) {
+    if (value !== this.password) {
+        throw new mongoose.MongooseError('Password does not match!');
+    }
+});
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

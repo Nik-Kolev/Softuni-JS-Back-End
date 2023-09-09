7ad30866c246f5161c35725d9f162aed23b7fe62
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-09-09 18:52:58 +0300

    small Photo service fix

--- Petstagram/src/services/photoServices.js
+++ Petstagram/src/services/photoServices.js
@@ -1,8 +1,6 @@
 const Photo = require('../models/Photo');
 
-exports.createPhoto = (photoData) => {
-    Photo.create(photoData);
-};
+exports.createPhoto = (photoData) => Photo.create(photoData);
 
 exports.getAll = () => Photo.find().populate('owner');
 

8732247770958c5e8eb2b245fd67b923d65e916b
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-09-30 19:45:34 +0300

    creature schema updated to meet the requirements

--- WizardCreation/src/models/Creature.js
+++ WizardCreation/src/models/Creature.js
@@ -3,27 +3,34 @@ const mongoose = require('mongoose')
 const creatureSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Name is required !'],
+        minLength: [2, 'Name should be at least 2 characters long !'],
     },
     species: {
         type: String,
-        required: true
+        required: [true, 'Species is required !'],
+        minLength: [3, 'Species should be at least 3 characters long !'],
     },
     skin: {
         type: String,
-        required: true
+        required: [true, 'Skin Color is required !'],
+        minLength: [3, 'Skin Color should be at least 3 characters long !'],
     },
     eye: {
         type: String,
-        required: true
+        required: [true, 'Eye Color is required !'],
+        minLength: [3, 'Eye Color should be at least 3 characters long !'],
     },
     imageUrl: {
         type: String,
-        required: true
+        required: [true, 'Image Url is required !'],
+        match: /^https?:\/\//i
     },
     description: {
         type: String,
-        required: true
+        required: [true, 'Description is required !'],
+        minLength: [5, 'Description should be at least 3 characters long !'],
+        maxLength: [500, 'Description should not exceed 00 characters !'],
     },
     votes: [{
         type: mongoose.Types.ObjectId,

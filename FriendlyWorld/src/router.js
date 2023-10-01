4f6ae659f60a0c2c84a3aede90e337017d8625ac
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2023-10-01 16:19:22 +0300

    404 page added

--- FriendlyWorld/src/router.js
+++ FriendlyWorld/src/router.js
@@ -4,4 +4,8 @@ const homeController = require('./controllers/homeController')
 
 router.use(homeController)
 
+router.get('*', (req, res) => {
+    res.redirect('/404')
+})
+
 module.exports = router
